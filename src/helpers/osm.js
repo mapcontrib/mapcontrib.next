1963a90f70b6246e2edb4b9bf6e55cd8d6482ed0
Romain Crestey <rcrestey@gmail.com>
2018-04-26 15:56:37 +0200

    fix: null check for token

--- src/helpers/osm.js
+++ src/helpers/osm.js
@@ -1,10 +1,15 @@
 export const computeId = (type, id) => `${type}/${id}`;
 
-const cleanToken = token => token.replace(/"|\\/gi, '');
+const cleanToken = token => token && token.replace(/"|\\/gi, '');
 
 export const getTokensFromLocalStorage = () => ({
-  apiToken: cleanToken(localStorage['https://api.openstreetmap.org/api/0.6oauth_token']),
-  apiTokenSecret: cleanToken(localStorage['https://api.openstreetmap.org/api/0.6oauth_token_secret']),
+  apiToken: cleanToken(
+    localStorage['https://api.openstreetmap.org/api/0.6oauth_token']
+  ),
+  apiTokenSecret: cleanToken(
+    localStorage['https://api.openstreetmap.org/api/0.6oauth_token_secret']
+  ),
   osmToken: localStorage['https://www.openstreetmap.orgoauth_token'],
-  osmTokenSecret: localStorage['https://www.openstreetmap.orgoauth_token_secret']
+  osmTokenSecret:
+    localStorage['https://www.openstreetmap.orgoauth_token_secret']
 });
